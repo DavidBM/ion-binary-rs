0f92a77966984661357785a3fb717abbf6e25373
Alejandro Herrera Guerrero <alejandroherreraguerrero@pop-os.localdomain>
2020-09-21 15:15:07 +0200

    Create good_tests and bad_tests folders

---
+++ src/tests/good_tests/mod.rs
@@ -0,0 +1,10 @@
+mod ion_parser_good;
+mod ion_parser_good_decimal;
+mod ion_parser_good_equivs;
+mod ion_parser_good_equivs_int;
+mod ion_parser_good_int;
+mod ion_parser_good_nop_padding;
+mod ion_parser_good_null;
+mod ion_parser_good_struct;
+mod ion_parser_good_timestamp;
+mod ion_parser_good_typecodes;
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,15 +1,7 @@
 mod binary_parser;
 mod ion_parser;
-mod ion_parser_good;
-mod ion_parser_good_decimal;
-mod ion_parser_good_equivs;
-mod ion_parser_good_equivs_int;
-mod ion_parser_good_int;
-mod ion_parser_good_nop_padding;
-mod ion_parser_good_null;
-mod ion_parser_good_struct;
-mod ion_parser_good_timestamp;
-mod ion_parser_good_typecodes;
+mod good_tests;
+mod bad_tests;
 
 #[macro_use]
 mod test_utils;
@@ -18,93 +10,6 @@ mod test_utils;
 NOTE: All current Ion Binnary tests.
 
 The ones with the "✓" are the ones we already have implemented
-
-✓	good/clobWithDel
-✓	good/intBigSize256
-✓	good/intBigSize14
-✓	good/structLen13
-✓	good/clobWithNonAsciiCharacter
-✓	good/clobWithNullCharacter
-✓	good/decimalNegativeOneDotZero
-✓	good/decimalNegativeZeroDot
-✓	good/decimalNegativeZeroDotZero
-✓	good/decimalOneDotZero
-✓	good/decimalZeroDot
-✓	good/emptyThreeByteNopPad
-✓	good/float32
-✓	good/intBigSize1201
-✓	good/intBigSize13
-✓	good/intBigSize16
-✓	good/intLongMaxValuePlusOne
-✓	good/intLongMinValue
-✓	good/item1
-✓	good/nopPad16Bytes
-✓	good/nopPadInsideEmptyStructNonZeroSymbolId
-✓	good/nopPadInsideEmptyStructZeroSymbolId
-✓	good/nopPadInsideStructWithNopPadThenValueNonZeroSymbolId
-✓	good/nopPadInsideStructWithNopPadThenValueZeroSymbolId
-✓	good/nopPadInsideStructWithValueThenNopPad
-✓	good/nopPadOneByte
-✓	good/valueBetweenNopPads
-✓	good/valueFollowedByNopPad
-✓	good/valuePrecededByNopPad
-✓	good/null
-✓	good/nullBlob
-✓	good/nullBool
-✓	good/nullClob
-✓	good/nullDecimal
-✓	good/nullFloat
-✓	good/nullInt2
-✓	good/nullInt3
-✓	good/nullList
-✓	good/nullSexp
-✓	good/nullString
-✓	good/nullStruct
-✓	good/nullSymbol
-✓	good/nullTimestamp
-✓	good/structAnnotatedEmpty
-✓	good/structAnnotatedOrdered
-✓	good/structEmpty
-✓	good/structLen14
-✓	good/structLen15
-✓	good/structOrdered
-✓	good/structUnordered
-✓	good/symbolExplicitZero
-✓	good/symbolImplicitZero
-✓	good/testfile28
-✓	good/equivs/intsLargeNegative1
-✓	good/equivs/intsLargeNegative2
-✓	good/equivs/intsLargeNegative3
-✓	good/equivs/intsLargePositive1
-✓	good/equivs/intsLargePositive2
-✓	good/equivs/intsLargePositive3
-✓	good/equivs/nopPadEmptyStruct
-✓	good/equivs/nopPadNonEmptyStruct
-✓	good/equivs/paddedInts
-✓	good/equivs/timestampFractions
-✓	good/equivs/timestampSuperfluousOffset
-✓	good/timestamp/timestamp2011
-✓	good/timestamp/timestamp2011-02
-✓   good/timestamp/timestamp2011-02-20
-✓   good/timestamp/timestamp2011-02-20T19_30_59_100-08_00
-✓   good/typecodes/T0
-✓   good/typecodes/T1
-✓   good/typecodes/T2
-✓   good/typecodes/T3
-✓   good/typecodes/T4
-✓   good/typecodes/T5
-✓   good/typecodes/T6-large
-✓   good/typecodes/T6-small
-✓   good/typecodes/T7-large
-✓   good/typecodes/T7-small
-✓   good/typecodes/T8
-✓   good/typecodes/T9
-✓   good/typecodes/T10
-✓   good/typecodes/T11
-✓   good/typecodes/T12
-✓   good/typecodes/T13
-✓   good/typecodes/T14
-✓   good/typecodes/T15
     ---
     bad/annotationLengthTooLongContainer
     bad/annotationLengthTooLongScalar
