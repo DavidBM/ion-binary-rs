2653f65905c97d83dd531446bc728adb694dc579
David Bonet <webbonet@gmail.com>
2020-10-12 00:43:17 +0200

    add annotation encode

--- src/ion_encoder.rs
+++ src/ion_encoder.rs
@@ -120,10 +120,47 @@ impl IonEncoder {
 
     pub(crate) fn encode_annotation(
         &mut self,
-        _annotations: &[String],
-        _value: &IonValue,
+        annotations: &[String],
+        value: &IonValue,
     ) -> Vec<u8> {
-        unimplemented!()
+        let mut annot_buffer: Vec<u8> = vec![];
+
+        for annot in annotations {
+            let annot_symbol = self.symbol_table.insert_symbol(annot);
+            let mut annot_symbol_bytes = encode_varuint(&annot_symbol.to_be_bytes());
+            annot_buffer.append(&mut annot_symbol_bytes);
+        }
+
+        let mut annot_len_bytes = encode_varuint(&annot_buffer.len().to_be_bytes());
+
+        let mut value_bytes = self.encode_value(value);
+
+        annot_len_bytes.append(&mut annot_buffer);
+
+        let mut buffer = annot_len_bytes;
+
+        buffer.append(&mut value_bytes);
+
+        let len = buffer.len();
+        let has_len_field = len >= ION_LEN_ON_HEADER_WHEN_EXTRA_LEN_FIELD_REQUIRED.into();
+
+        let mut header = 0xE0;
+
+        let mut final_buffer: Vec<u8> = vec![];
+
+        if has_len_field {
+            header += ION_LEN_ON_HEADER_WHEN_EXTRA_LEN_FIELD_REQUIRED;
+            final_buffer.push(header);
+            let mut len_bytes = encode_varuint(&len.to_be_bytes());
+            final_buffer.append(&mut len_bytes);
+            final_buffer.append(&mut buffer);
+        } else {
+            header += u8::try_from(len).unwrap();
+            final_buffer.push(header);
+            final_buffer.append(&mut buffer);
+        };
+
+        final_buffer
     }
 
     pub(crate) fn encode_struct(&mut self, value: &HashMap<String, IonValue>) -> Vec<u8> {
