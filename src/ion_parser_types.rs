1cd59762361d0e478901eab75b1ac6c9bbf86c29
David Bonet <bonet@hey.com>
2020-11-16 17:29:55 +0100

    Add error descriptions

--- src/ion_parser_types.rs
+++ src/ion_parser_types.rs
@@ -1,45 +1,78 @@
+use std::error::Error;
 use crate::binary_parser_types::*;
 use crate::symbol_table::SymbolContextError;
 use bigdecimal::BigDecimal;
 use chrono::{DateTime, FixedOffset};
 use num_bigint::BigInt;
 use std::collections::HashMap;
+use thiserror::Error;
 
 /// Indicated a problem in the binary blob internal structure. When all data is read
 /// the library will return IonParserError::BinaryError(ParsingError::NoDataToRead).
-#[derive(Eq, PartialEq, Debug)]
+#[derive(PartialEq, Debug, Error)]
 pub enum IonParserError {
+    #[error("Ion type not implemented")]
     Unimplemented,
-    BadFormatLengthFound,
+    #[error("Null annotation found")]
     NullAnnotationFound,
+    #[error("Nested annotation found")]
     NestedAnnotations,
+    #[error("Annotation len field is wrong")]
     BadAnnotationLength,
+    #[error("Ion symbol table declared as local and shared at the same time")]
     SharedTableAndLocalTableDeclarationIntTheSameAnnotation,
+    #[error("Not known symbol id")]
     SymbolIdNotDefined,
+    #[error("Local table structure definition is not correct")]
     LocalTableWithoutInternalStruct,
+    #[error("The shared table is missing it's name")]
     SharedTableDefinitionWithoutName,
+    #[error("Error adding the shared table into the context")]
     ErrorAddingSharedTableToContext(SymbolContextError),
+    #[error("Error importing local table")]
     ErrorAddingCreatingLocal(SymbolContextError),
+    #[error("The local table don't have a valid import")]
     LocalSymbolTableWithoutValidImport,
+    #[error("Symbol id not found")]
     SymbolNotFoundInTable,
+    #[error("The list content is longer than what it's len field indicates")]
     ListLengthWasTooShort,
+    #[error("Ion String is not valid UTF-8")]
     NonUtf8String,
+    #[error("Internal error: Open a bug on github")]
     DidNotGetAListConsumingAListThisIsABug,
+    #[error("Symbol id is bigger than usize")]
     SymbolIdTooBig,
+    #[error("Table version bigger than usize")]
     TableVersionTooBig,
+    #[error("Date value is far too big for a date")]
     DateValueTooBig,
+    #[error("The second fraction is bigger than 1 second")]
     DateSecondFractionOverflow,
+    #[error("The second fraction is negative")]
     DateNegativeSecondFraction,
+    #[error("The date has hours defined but not minutes, which is illegal")]
     DateWithHourButNoMinutes,
+    #[error("The length of the value is bigger than usize, which is far too")]
     ValueLenTooBig,
+    #[error("Floats can only be 4 or 8 bytes")]
     NotValidLengthFloat,
+    #[error("Error parsing the ion binary format")]
     BinaryError(ParsingError),
+    #[error("Exponent for the decimal value is too big (greater than i64)")]
     DecimalExponentTooBig,
+    #[error("Bool cannot have len")]
     InvalidBoolLength(ValueLength),
+    #[error("The date is not valid")]
     InvalidDate(i32, u32, u32, u32, u32, u32, u32),
+    #[error("Ion type 15 doesn't exist in Ion 1.0")]
     InvalidReservedTypeDescriptor,
+    #[error("Negative ints need a value")]
     InvalidNegativeInt,
+    #[error("Ordered structs cannot be empty")]
     EmptyOrderedStruct,
+    #[error("Error transforming the IonValue to a rust type")]
+    ValueExtractionFailure(IonExtractionError)
 }
 
 impl From<ParsingError> for IonParserError {
