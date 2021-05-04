b9d8628e37c64bfd56e00970f38e1e7f14aed62e
Alejandro Herrera Guerrero <alejandro.herrera@couragium.com>
2021-05-04 13:35:48 +0200

    Add some bad tests structure

--- src/tests/bad_tests/ion_parser_bad_serde_value.rs
+++ src/tests/bad_tests/ion_parser_bad_serde_value.rs
@@ -1,1 +1,15 @@
+use crate::IonValue;
 
+#[test]
+fn serde_from_ion_symbol() {
+    let value = IonValue::Symbol("any".to_string());
+}
+
+#[test]
+fn serde_from_ion_list() {}
+
+#[test]
+fn ion_from_serde_symbol() {}
+
+#[test]
+fn ion_from_serde_list() {}
