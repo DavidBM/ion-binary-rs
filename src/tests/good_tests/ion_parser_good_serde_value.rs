1588dd1f0ad934507e892ffdd524f4ada5287a27
Alejandro Herrera Guerrero <alejandro.herrera@couragium.com>
2021-05-04 10:36:56 +0200

    Add some test definitions

---
+++ src/tests/good_tests/ion_parser_good_serde_value.rs
@@ -0,0 +1,81 @@
+
+#[test]
+fn serde_from_ion_null() {
+
+}
+
+#[test]
+fn serde_from_ion_bool() {
+
+}
+
+#[test]
+fn serde_from_ion_integer() {
+
+}
+
+#[test]
+fn serde_from_ion_big_integer() {
+
+}
+
+#[test]
+fn serde_from_ion_float() {
+
+}
+
+#[test]
+fn serde_from_ion_string() {
+
+}
+
+#[test]
+fn serde_from_ion_list() {
+
+}
+
+#[test]
+fn serde_from_ion_struct() {
+
+}
+
+
+#[test]
+fn ion_from_serde_null() {
+
+}
+
+#[test]
+fn ion_from_serde_bool() {
+
+}
+
+#[test]
+fn ion_from_serde_integer() {
+
+}
+
+#[test]
+fn ion_from_serde_big_integer() {
+
+}
+
+#[test]
+fn ion_from_serde_float() {
+
+}
+
+#[test]
+fn ion_from_serde_string() {
+
+}
+
+#[test]
+fn ion_from_serde_list() {
+
+}
+
+#[test]
+fn ion_from_serde_struct() {
+
+}
